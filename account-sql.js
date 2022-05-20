8ad3fa80adba541883a6e1af05dd04b59910a766
Nikita <nikitann2501@gmail.com>
2022-05-20 15:58:03 +0530

    output success

--- account-sql.js
+++ account-sql.js
@@ -1,94 +1,128 @@
 const { connection } = require('./db');
 
-const dropQuery = `drop table if exists accounts`
+const dropTableFunction = (dropQuery) => {
+    try {
+        connection.query(dropQuery, (err) => {
+            if (!err) {
+                console.log('Table dropped: accounts');
+                return 'Table dropped: accounts'
+            } else {
+                console.log('error occured: ', err);
+                return err;
+            }
 
-connection.query(dropQuery, (err) => {
-    if (!err) {
-        console.log('table dropped: accounts');
-    } else {
-        console.log('error occured: ', err);
+        });
+    } catch (e) {
+        console.log('error: ', e);
+        return e;
     }
 
-});
+}
 
-const createTableQuery = `create table accounts (
-    user_id serial primary key not null,
-    user_name text not null,
-    email text not null,
-    acc_balance real default 0.0,
-    acc_type varchar (10) default 'SB'
-    );`
-
-connection.query(createTableQuery, (err) => {
-    if (!err) {
-        console.log('table created: accounts');
-    } else {
-        console.log('error occured: ', err);
+const createFunction = (createTableQuery) => {
+    try {
+        connection.query(createTableQuery, (err) => {
+            if (!err) {
+                console.log('table created: accounts');
+                return 'Table created: accounts';
+            } else {
+                console.log('error occured: ', err);
+                return err;
+            }
+        });
+    } catch (e) {
+        console.log('error: ', e);
+        return e;
     }
-});
 
-var data = [`('Nikita Nandani', 'nikita@bdec.in', 1500);`, `('Shreya Prasad', 'shreya@abc.in', 2000);`, `('Asyush Aman', 'ayush@aman.in', 50);`, `('Shruti', 'shru@gmail.in', 100);`, `('Ved Prakash', 'nikita@bdec.in', 1070);`]
-
-for (let i = 0; i < data.length; i++) {
-    let insertDataQuery = `insert into accounts (user_name, email, acc_balance) values ${data[i]}`
-    connection.query(insertDataQuery, (err) => {
-        if (!err) {
-            console.log('data added, row: ' + (i + 1));
-        } else {
-            console.log('error occured: ', err);
-        }
-    });
 }
 
-const findQuery = `select * from accounts order by user_id asc;`
+const insertFunction = (columns, data) => {
 
-const findFunction = () => {
-    connection.query(findQuery, (err, res) => {
-        if (!err) {
-            console.log(res.rows);
-        } else {
-            console.log('error occured: ', err);
+    try {
+        for (let i = 0; i < data.length; i++) {
+            let insertDataQuery = `insert into accounts (${columns}) values (${data[i]});`
+            connection.query(insertDataQuery, (err) => {
+                if (!err) {
+                    console.log('data added, row: ' + (i + 1));
+                } else {
+                    console.log('error occured: ', err);
+                }
+            });
         }
-    });
+        return 'Inserted'
+
+    } catch (e) {
+        console.log('error: ', e);
+        return e;
+    }
+
 }
 
-findFunction();
+const findFunction = async (findQuery) => {
+    try {
+        // var data = connection.query(findQuery, (err, res) => {
+        //     if (!err) {
+        //         console.log(res.rows);
+        //         var data = res.rows;
+        //         return data;
+        //     } else {
+        //         console.log('error occured: ', err);
+        //         return err;
+        //     }
+        // });
+        var data = await connection.query(findQuery);
+        // console.log(data.rows);
+        return data.rows;
+    } catch (e) {
+        console.log('error: ', e);
+        return e;
+    }
+
+}
 
 const updateFunction = (updateQuery) => {
-    connection.query(updateQuery, (err, res) => {
-        if (!err) {
-            console.log('Table updated. \nRows affected: ' + res.rowCount);
-        } else {
-            console.log('error occured: ', err);
-        }
-    });
+    try {
+        connection.query(updateQuery, (err, res) => {
+            if (!err) {
+                console.log('Table updated. \nRows affected: ' + res.rowCount);
+                return 'Table updated. \nRows affected: ' + res.rowCount;
+            } else {
+                console.log('error occured: ', err);
+                return err;
+            }
+        });
+    } catch (e) {
+        console.log('error: ', e);
+        return e;
+    }
+
 }
-
-const updateQuery1 = `update accounts
-    set user_name = 'Ayush Aman'
-    where email = 'ayush@aman.in'`;
-
-updateFunction(updateQuery1);
-findFunction();
-
-const updateQuery2 = `update accounts
-    set acc_type = 'CURRENT'
-    where acc_type = 'SB'`;
-
-updateFunction(updateQuery2);
-findFunction();
-
-const deleteQuery = `delete from accounts
-    where acc_balance = 1500`;
 
 const deleteFunction = (deleteQuery) => {
-    connection.query(deleteQuery, (err, res) => {
-        if (!err) {
-            console.log('Request data deleted. \nRows affected: ' + res.rowCount);
-        } else {
-            console.log('error occured: ', err);
-        }
-    });
+    try {
+        connection.query(deleteQuery, (err, res) => {
+            if (!err) {
+                console.log('Request data deleted. \nRows affected: ' + res.rowCount);
+                return 'Request data deleted. \nRows affected: ' + res.rowCount;
+            } else {
+                console.log('error occured: ', err);
+                return err;
+            }
+        });
+    } catch (e) {
+        console.log('error: ', e);
+        return e;
+    }
+
 }
-deleteFunction(deleteQuery);
-findFunction();
+
+
+module.exports = {
+    dropTableFunction,
+    createFunction,
+    insertFunction,
+    findFunction,
+    updateFunction,
+    deleteFunction
+}
--- app.js
+++ app.js
@@ -1,14 +1,55 @@
 require('./db');
 
-require('./account-sql')
+const crud = require('./account-sql')
 const express = require('express');
 const app = express();
 const port = 8080;
+const path = require('path')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
+});
+
+
+
+
+app.get('/', async (req, res) => {
+
+  const dropQuery = `drop table if exists accounts`
+
+  const createTableQuery = `create table accounts (
+  user_id serial primary key not null,
+  user_name text not null,
+  email text not null,
+  acc_balance real default 0.0,
+  acc_type varchar (10) default 'SB'
+  );`
+
+  const findQuery = `select * from accounts order by user_id asc;`
+
+  const columns = `user_name, email, acc_balance`
+
+  var data = [`'Nikita Nandani', 'nikita@bdec.in', 1500`, `'Shreya Prasad', 'shreya@abc.in', 2000`, `'Asyush Aman', 'ayush@aman.in', 50`, `'Shruti', 'shru@gmail.in', 100`, `'Ved Prakash', 'nikita@bdec.in', 1070`];
+
+  const updateQuery1 = `update accounts
+  set user_name = 'Ayush Aman'
+  where email = 'ayush@aman.in'`;
+
+  const updateQuery2 = `update accounts
+  set acc_type = 'CURRENT'
+  where acc_type = 'SB'`;
+
+  const deleteQuery = `delete from accounts
+  where acc_balance = 1500`;
+
+  await crud.dropTableFunction(dropQuery);
+  await crud.createFunction(createTableQuery);
+  await crud.insertFunction(columns, data);
+  await crud.updateFunction(updateQuery1);
+  await crud.updateFunction(updateQuery2);
+  await crud.deleteFunction(deleteQuery);
+  await crud.findFunction(findQuery);
+  let result = await crud.findFunction(findQuery);
+  res.send(result)
 });
